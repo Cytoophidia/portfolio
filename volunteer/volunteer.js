a00a310810bed9425005d4aa2000d9586ec2a50d
Cytoophidia <90016472+Cytoophidia@users.noreply.github.com>
2022-05-09 15:57:51 -0400

    Add files via upload

--- volunteer/volunteer.js
+++ volunteer/volunteer.js
@@ -13,14 +13,15 @@ project.addEventListener("click", event=>{
     location.href = "../projects/project.html"
 })
 
-let VolunteerWorks = document.querySelector("#VolunteerWorks")
-VolunteerWorks.addEventListener("click", event=>{
-    alert("Coming Soon!")
-})
 
 let week5 = document.querySelector("#week5")
 week5.addEventListener("click", event=>{
     location.href = "assignments/week5.html"
+})
+
+let week6 = document.querySelector("#week6")
+week6.addEventListener("click", event=>{
+    location.href = "assignments/week6-audio.html"
 })
 
 let week7 = document.querySelector("#week7")
@@ -28,7 +29,23 @@ week7.addEventListener("click", event=>{
     location.href = "assignments/week7-motiongraphic.html"
 })
 
+let week8 = document.querySelector("#week8")
+week8.addEventListener("click", event=>{
+    location.href = "assignments/week8-visualdesign.html"
+})
+
+
 let week10 = document.querySelector("#week10")
 week10.addEventListener("click", event=>{
     location.href = "assignments/week10-game.html"
+})
+
+let week11 = document.querySelector("#week11")
+week11.addEventListener("click", event=>{
+    location.href = "assignments/week11-datavisualization.html"
+})
+
+let week12 = document.querySelector("#week12")
+week12.addEventListener("click", event=>{
+    location.href = "assignments/week12-instructionaldesign.html"
 })
